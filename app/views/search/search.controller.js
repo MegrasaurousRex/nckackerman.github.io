4f4538c65439f8cb718de49f8db6f64b3afc98be
nick ackerman <nckackerman@gmail.com>
2016-10-04 18:33:43 -0500

    initial search service added

--- app/views/search/search.controller.js
+++ app/views/search/search.controller.js
@@ -1,8 +1,10 @@
 (function() {
     'use strict';
     angular.module('app').controller('SearchController', SearchController);
+    SearchController.$inject = ['searchService'];
 
     function SearchController() {
         var vm = this;
+
     }
 })();
