56401ca0a2800e2fdb64dc87a90e18622dfc9281
nick ackerman <nckackerman@gmail.com>
2016-10-05 00:39:35 -0500

    initial commit for autocomplete search and search product display

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
 (function() {
     'use strict';
-    angular.module('app', ['ui.router', 'algoliasearch']);
+    angular.module('app', ['ui.router', 'algoliasearch', 'algolia.autocomplete']);
 })();
--- app/views/search/search.service.js
+++ app/views/search/search.service.js
@@ -9,13 +9,13 @@
         };
         return factory;
 
+        //TODO: should also take in a success and failure call back to better communicate with the controller
         function algoliaSearch(serchTerm) {
 
             var client = algoliasearch('16KBF0AU4T', '449d73ff5b0940fecddb1c144c2baaae');
             var index = client.initIndex('bestbuy_data');
             index.search(serchTerm, function searchDone(err, content) {
-                console.log(err, content);
-                return content;
+                return content.hits;
             });
         }
     }
